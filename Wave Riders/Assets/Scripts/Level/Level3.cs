59cb4f0875b4d342bbd652e55f465b7dd1bc59f2
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2025-04-21 11:05:03 -0400

    Level 3 Coin Trail Implemented

--- Wave Riders/Assets/Scripts/Level/Level3.cs
+++ Wave Riders/Assets/Scripts/Level/Level3.cs
@@ -7,6 +7,8 @@ public class Level3 : Level
     [SerializeField] private GameObject Jump;
     [SerializeField] private GameObject Shield;
 
+    [SerializeField] private List<GameObject> CoinGroups;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -26,6 +28,60 @@ public class Level3 : Level
         else if(powerUpInt == 2)
         {
             Shield.SetActive(true);
+        }
+
+        PlaceCoins(0, powerUpInt);
+    }
+
+    public override void PlaceCoins(int obsInt, int puInt)
+    {
+        System.Random rand = new System.Random();
+        int coinInt = rand.Next(0, 3);
+
+        if (coinInt != 0)
+        {
+            CoinGroups[0].SetActive(true);
+        }
+
+        if (puInt == 1)
+        {
+            if (coinInt == 1)
+            {
+                CoinGroups[3].SetActive(true);
+            }
+            
+            if(coinInt == 2)
+            {
+                CoinGroups[2].SetActive(true);
+                CoinGroups[4].SetActive(true);
+            }
+        }
+        else if (puInt == 2)
+        {
+            if (coinInt == 1)
+            {
+                CoinGroups[4].SetActive(true);
+            }
+
+            if (coinInt == 2)
+            {
+                CoinGroups[1].SetActive(true);
+                CoinGroups[3].SetActive(true);
+            }
+        }
+        else
+        {
+            if (coinInt == 1)
+            {
+                CoinGroups[1].SetActive(true);
+                CoinGroups[4].SetActive(true);
+            }
+
+            if (coinInt == 2)
+            {
+                CoinGroups[2].SetActive(true);
+                CoinGroups[3].SetActive(true);
+            }
         }
     }
 }
